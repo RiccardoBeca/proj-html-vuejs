0ef9800311e6ddb9b88c3b61960962f95042aa2b
RiccardoBeca <riccardobeca@gmail.com>
2022-05-30 18:14:12 +0100

    footer done

--- src/components/BlogComp.vue
+++ src/components/BlogComp.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    BLOGCOMP
+    <div class="container w-70 d-flex py-5">
+      <div class="row">
+        <div class="col">
+          <img src="../assets/img/h-2-blog-img-1.jpg" alt="">
+          <h4>This way, loves: a detail guide through new design</h4>
+          <p><em>By Emily Fields</em></p>
+        </div>
+        <div class="col">
+          <img src="../assets/img/h-2-blog-img-2.jpg" alt="">
+          <h4>I try to give people a different way of looking art</h4>
+          <p><em>By Emily Fields</em></p>
+        </div>
+        <div class="col">
+          <img src="../assets/img/h-2-blog-img-3.jpg" alt="">
+          <h4>Introduce Richard Laperri&eacute;rre of those amazing features</h4>
+          <p><em>By Emily Fields</em></p>
+        </div>
+      </div>      
+    </div>
   </div>
 </template>
 
@@ -14,4 +32,17 @@ export default {
 @import '../assets/style/_vars.scss';
 @import '../assets/style/_general.scss';
 @import '../assets/style/_utils.scss';
+
+.row{
+  padding: 50px 0px;
+}
+.col img{
+  width: 400px;
+}
+
+h4{
+  font-size: 20px;
+  padding-top: 20px;
+}
+
 </style>
--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
-    FOOTER
+    <div class="container w-70">
+      <div class="row">
+        <div class="col">
+          <img src="../assets/img/logo-sidearea-1.png" alt="">
+          <p>Let's get creative</p>
+        </div>
+        <div class="col">
+          <span>maree.qode@gmail.com</span>
+          <span>+44645 321 789</span>
+        </div>
+        <div class="col">
+          <span>Avenue d'Auderghem 10</span>
+          <span>1040 Brussels, Belgium</span>
+        </div>
+        <div class="col">
+          <span>Stay in touch with us</span>
+          <i class="fa-brands fa-twitter"></i>
+          <i class="fa-brands fa-pinterest-p"></i>
+          <i class="fa-brands fa-facebook-f"></i>
+          <i class="fa-brands fa-linkedin-in"></i>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,4 +36,20 @@ export default {
 @import '../assets/style/_vars.scss';
 @import '../assets/style/_general.scss';
 @import '../assets/style/_utils.scss';
+
+div{
+  background-color: #c0e1cf;
+  padding:10px 20px;
+  img{
+    padding-bottom: 5px;
+  }
+  span{
+    display: block;
+  }
+  i{
+    padding-top: 5px;
+    padding-right: 30px;
+    cursor: pointer;
+  }
+}
 </style>
